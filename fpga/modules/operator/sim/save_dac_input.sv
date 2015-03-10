2e7523f5e2fba0d42ce6f0acd074f2c374af1917
Greg Taylor <gtaylor@sonic.net>
2015-03-10 11:45:25 -0400

    Removed old caltech copyright

--- fpga/modules/operator/sim/save_dac_input.sv
+++ fpga/modules/operator/sim/save_dac_input.sv
@@ -88,19 +88,3 @@ module save_dac_input #(
         end
     endprogram
 endmodule
-
-/*******************************************************************************
-#
-#   Copyright 2012, by the California Institute of Technology.
-#   ALL RIGHTS RESERVED. United States Government Sponsorship acknowledged.
-#   Any commercial use must be negotiated with the Office of Technology
-#   Transfer at the California Institute of Technology.
-#
-#   This software may be subject to U.S. export control laws and regulations.
-#   By accepting this document, the user agrees to comply with all applicable
-#   U.S. export laws and regulations.  User has the responsibility to obtain
-#   export licenses, or other export authority as may be required before
-#   exporting such information to foreign countries or providing access to
-#   foreign persons.
-#
-#******************************************************************************/
