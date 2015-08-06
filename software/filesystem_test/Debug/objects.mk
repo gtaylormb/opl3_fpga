f719b38b922ebf792ce01b41e6ac01757c28819e
Greg Taylor <gtaylor@sonic.net>
2015-08-06 01:54:25 -0400

    Remove filesystem_test project

--- software/filesystem_test/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -Wl,--start-group,-lxil,-lgcc,-lc,-lstdc++,--end-group -Wl,--start-group,-lxilffs,-lxil,-lgcc,-lc,--end-group -Wl,--start-group,-lrsa,-lxil,-lgcc,-lc,--end-group
-
