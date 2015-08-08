98a6069425284fb49be84250cbe0d76b32b0da4e
Greg Taylor <gtaylor@sonic.net>
2015-08-08 10:46:07 -0400

    Added release software pieces

---
+++ software/imfplay_port/Release/sources.mk
@@ -0,0 +1,28 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+LD_SRCS := 
+C_UPPER_SRCS := 
+CXX_SRCS := 
+OBJ_SRCS := 
+S_SRCS := 
+CC_SRCS := 
+C_SRCS := 
+CPP_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
+CC_DEPS := 
+EXECUTABLES := 
+OBJS := 
+C_UPPER_DEPS := 
+CXX_DEPS := 
+S_UPPER_DEPS := 
+C_DEPS := 
+CPP_DEPS := 
+ELFSIZE := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+src \
+
