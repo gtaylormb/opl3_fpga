3bf40ddf1e777d42f85c594902ad4df3b8d83927
Greg Taylor <gtaylor@sonic.net>
2024-04-02 12:05:11 -0400

    downgrade warnings, .xsa file now built

--- fpga/scripts/vivado_bd.tcl
+++ fpga/scripts/vivado_bd.tcl
@@ -10,6 +10,12 @@ update_ip_catalog -rebuild
 set_property target_language Verilog [current_project]
 set_property default_lib work [current_project]
 
+# downgrade critial warnings regarding https://digilent.com/reference/programmable-logic/zybo-z7/reference-manual?redirect=1#hardware_errata
+set_msg_config -id "PSU-1" -new_severity "INFO"
+set_msg_config -id "PSU-2" -new_severity "INFO"
+set_msg_config -id "PSU-3" -new_severity "INFO"
+set_msg_config -id "PSU-4" -new_severity "INFO"
+
 source ${BD_SRC}
 
 save_bd_design_as -dir $outputDir -force design_1.bd
