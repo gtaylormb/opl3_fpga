51426a04bf5119704984ef4901fc2a8167e86b57
Greg Taylor <gtaylor@sonic.net>
2024-04-10 20:37:47 -0400

    combined 4 channel acc into right left accumulators, area savings

--- fpga/modules/channels/src/channels.sv
+++ fpga/modules/channels/src/channels.sv
@@ -54,14 +54,12 @@ module channels
 );
     localparam CHAN_2_OP_WIDTH = OP_OUT_WIDTH + 1;
     localparam CHAN_4_OP_WIDTH = OP_OUT_WIDTH + 2;
-    localparam CHANNEL_ACCUMULATOR_WIDTH = CHAN_4_OP_WIDTH + 8*2 - 1;
+    localparam CHANNEL_ACCUMULATOR_WIDTH = CHAN_4_OP_WIDTH + 8*2;
 
     operator_out_t operator_out;
     logic signed [OP_OUT_WIDTH-1:0] operator_mem_out;
-    logic signed [SAMPLE_WIDTH-1:0] channel_a = 0;
-    logic signed [SAMPLE_WIDTH-1:0] channel_b = 0;
-    logic signed [SAMPLE_WIDTH-1:0] channel_c = 0;
-    logic signed [SAMPLE_WIDTH-1:0] channel_d = 0;
+    logic signed [SAMPLE_WIDTH-1:0] channel_l = 0;
+    logic signed [SAMPLE_WIDTH-1:0] channel_r = 0;
     logic channel_valid = 0;
     logic ops_done_pulse;
     logic [REG_CONNECTION_SEL_WIDTH-1:0] connection_sel = 0;
@@ -147,10 +145,8 @@ module channels
         logic bank_num;
         logic [$clog2(NUM_OPERATORS_PER_BANK)-1:0] op_num;
         logic [$clog2(NUM_CHANNELS_PER_BANK)-1:0] channel_num;
-        logic signed [CHANNEL_ACCUMULATOR_WIDTH-1:0] channel_a_acc_pre_clamp;
-        logic signed [CHANNEL_ACCUMULATOR_WIDTH-1:0] channel_b_acc_pre_clamp;
-        logic signed [CHANNEL_ACCUMULATOR_WIDTH-1:0] channel_c_acc_pre_clamp;
-        logic signed [CHANNEL_ACCUMULATOR_WIDTH-1:0] channel_d_acc_pre_clamp;
+        logic signed [CHANNEL_ACCUMULATOR_WIDTH-1:0] channel_l_acc_pre_clamp;
+        logic signed [CHANNEL_ACCUMULATOR_WIDTH-1:0] channel_r_acc_pre_clamp;
     } self = 0, next_self;
 
     struct packed {
@@ -160,6 +156,10 @@ module channels
         logic signed [CHAN_2_OP_WIDTH-1:0] channel_2_op;
         logic signed [CHAN_4_OP_WIDTH-1:0] channel_4_op;
         logic latch_channels;
+        logic add_a;
+        logic add_b;
+        logic add_c;
+        logic add_d;
     } signals;
 
     always_ff @(posedge clk)
@@ -205,36 +205,36 @@ module channels
 
             if (self.channel_num < 3) begin
                 if (self.bank_num == 0) begin
-                    if (!is_new || (cha && !connection_sel[self.channel_num]))
-                        next_self.channel_a_acc_pre_clamp = self.channel_a_acc_pre_clamp + signals.channel_2_op;
-                    if (!is_new || (chb && !connection_sel[self.channel_num]))
-                        next_self.channel_b_acc_pre_clamp = self.channel_b_acc_pre_clamp + signals.channel_2_op;
-                    if (!is_new || (chc && !connection_sel[self.channel_num]))
-                        next_self.channel_c_acc_pre_clamp = self.channel_c_acc_pre_clamp + signals.channel_2_op;
-                    if (!is_new || (chd && !connection_sel[self.channel_num]))
-                        next_self.channel_d_acc_pre_clamp = self.channel_d_acc_pre_clamp + signals.channel_2_op;
+                    signals.add_a = !is_new || (cha && !connection_sel[self.channel_num]);
+                    signals.add_b = !is_new || (chb && !connection_sel[self.channel_num]);
+                    signals.add_c = !is_new || (chc && !connection_sel[self.channel_num]);
+                    signals.add_d = !is_new || (chd && !connection_sel[self.channel_num]);
                 end
                 else begin
-                    if (cha && !connection_sel[self.channel_num+3])
-                        next_self.channel_a_acc_pre_clamp = self.channel_a_acc_pre_clamp + signals.channel_2_op;
-                    if (chb && !connection_sel[self.channel_num+3])
-                        next_self.channel_b_acc_pre_clamp = self.channel_b_acc_pre_clamp + signals.channel_2_op;
-                    if (chc && !connection_sel[self.channel_num+3])
-                        next_self.channel_c_acc_pre_clamp = self.channel_c_acc_pre_clamp + signals.channel_2_op;
-                    if (chd && !connection_sel[self.channel_num+3])
-                        next_self.channel_d_acc_pre_clamp = self.channel_d_acc_pre_clamp + signals.channel_2_op;
+                    signals.add_a = cha && !connection_sel[self.channel_num+3];
+                    signals.add_b = chb && !connection_sel[self.channel_num+3];
+                    signals.add_c = chc && !connection_sel[self.channel_num+3];
+                    signals.add_d = chd && !connection_sel[self.channel_num+3];
                 end
             end
             else begin
-                if ((self.bank_num == 0 && !is_new) || cha)
-                    next_self.channel_a_acc_pre_clamp = self.channel_a_acc_pre_clamp + signals.channel_2_op;
-                if ((self.bank_num == 0 && !is_new) || chb)
-                    next_self.channel_b_acc_pre_clamp = self.channel_b_acc_pre_clamp + signals.channel_2_op;
-                if ((self.bank_num == 0 && !is_new) || chc)
-                    next_self.channel_c_acc_pre_clamp = self.channel_c_acc_pre_clamp + signals.channel_2_op;
-                if ((self.bank_num == 0 && !is_new) || chd)
-                    next_self.channel_d_acc_pre_clamp = self.channel_d_acc_pre_clamp + signals.channel_2_op;
+                signals.add_a = (self.bank_num == 0 && !is_new) || cha;
+                signals.add_b = (self.bank_num == 0 && !is_new) || chb;
+                signals.add_c = (self.bank_num == 0 && !is_new) || chc;
+                signals.add_d = (self.bank_num == 0 && !is_new) || chd;
             end
+
+            unique case ({signals.add_a, signals.add_c})
+            'b01, 'b10: next_self.channel_l_acc_pre_clamp = self.channel_l_acc_pre_clamp + signals.channel_2_op;
+            'b11:       next_self.channel_l_acc_pre_clamp = self.channel_l_acc_pre_clamp + signals.channel_2_op*2;
+            default:;
+            endcase
+
+            unique case ({signals.add_b, signals.add_d})
+            'b01, 'b10: next_self.channel_r_acc_pre_clamp = self.channel_r_acc_pre_clamp + signals.channel_2_op;
+            'b11:       next_self.channel_r_acc_pre_clamp = self.channel_r_acc_pre_clamp + signals.channel_2_op*2;
+            default:;
+            endcase
 
             if (self.channel_num == NUM_CHANNELS_PER_BANK - 1) begin
                 if (self.bank_num == 1) begin
@@ -291,25 +291,33 @@ module channels
             endcase
 
             if (self.bank_num == 0) begin
-                if (cha && connection_sel[self.channel_num])
-                    next_self.channel_a_acc_pre_clamp = self.channel_a_acc_pre_clamp + signals.channel_4_op;
-                if (chb && connection_sel[self.channel_num])
-                    next_self.channel_b_acc_pre_clamp = self.channel_b_acc_pre_clamp + signals.channel_4_op;
-                if (chc && connection_sel[self.channel_num])
-                    next_self.channel_c_acc_pre_clamp = self.channel_c_acc_pre_clamp + signals.channel_4_op;
-                if (chd && connection_sel[self.channel_num])
-                    next_self.channel_d_acc_pre_clamp = self.channel_d_acc_pre_clamp + signals.channel_4_op;
+                signals.add_a = cha && connection_sel[self.channel_num];
+                signals.add_b = chb && connection_sel[self.channel_num];
+                signals.add_c = chc && connection_sel[self.channel_num];
+                signals.add_d = chd && connection_sel[self.channel_num];
             end
             else begin
-                if (cha && connection_sel[self.channel_num+3])
-                    next_self.channel_a_acc_pre_clamp = self.channel_a_acc_pre_clamp + signals.channel_4_op;
-                if (chb && connection_sel[self.channel_num+3])
-                    next_self.channel_b_acc_pre_clamp = self.channel_b_acc_pre_clamp + signals.channel_4_op;
-                if (chc && connection_sel[self.channel_num+3])
-                    next_self.channel_c_acc_pre_clamp = self.channel_c_acc_pre_clamp + signals.channel_4_op;
-                if (chd && connection_sel[self.channel_num+3])
-                    next_self.channel_d_acc_pre_clamp = self.channel_d_acc_pre_clamp + signals.channel_4_op;
+                signals.add_a = cha && connection_sel[self.channel_num+3];
+                signals.add_b = chb && connection_sel[self.channel_num+3];
+                signals.add_c = chc && connection_sel[self.channel_num+3];
+                signals.add_d = chd && connection_sel[self.channel_num+3];
             end
+
+            /*
+            * The 4 16-bit output channels are normally combined into right and left in the analog domain
+            * after the YAC512 DAC outputs. Here we'll just add digitally.
+            */
+            unique case ({signals.add_a, signals.add_c})
+            'b01, 'b10: next_self.channel_l_acc_pre_clamp = self.channel_l_acc_pre_clamp + signals.channel_4_op;
+            'b11:       next_self.channel_l_acc_pre_clamp = self.channel_l_acc_pre_clamp + signals.channel_4_op*2;
+            default:;
+            endcase
+
+            unique case ({signals.add_b, signals.add_d})
+            'b01, 'b10: next_self.channel_r_acc_pre_clamp = self.channel_r_acc_pre_clamp + signals.channel_4_op;
+            'b11:       next_self.channel_r_acc_pre_clamp = self.channel_r_acc_pre_clamp + signals.channel_4_op*2;
+            default:;
+            endcase
 
             if (self.channel_num == 2) begin
                 if (self.bank_num == NUM_BANKS - 1)
@@ -341,36 +349,22 @@ module channels
      */
     always_ff @(posedge clk)
         if (signals.latch_channels) begin
-            if (self.channel_a_acc_pre_clamp > 2**(SAMPLE_WIDTH - 1) - 1)
-                channel_a <= 2**(SAMPLE_WIDTH - 1) - 1;
-            else if (self.channel_a_acc_pre_clamp < -2**(SAMPLE_WIDTH - 1))
-                channel_a <= -2**(SAMPLE_WIDTH - 1);
+            if (self.channel_l_acc_pre_clamp > 2**(SAMPLE_WIDTH - 1) - 1)
+                channel_l <= 2**(SAMPLE_WIDTH - 1) - 1;
+            else if (self.channel_l_acc_pre_clamp < -2**(SAMPLE_WIDTH - 1))
+                channel_l <= -2**(SAMPLE_WIDTH - 1);
             else
-                channel_a <= self.channel_a_acc_pre_clamp;
+                channel_l <= self.channel_l_acc_pre_clamp;
 
-            if (self.channel_b_acc_pre_clamp > 2**(SAMPLE_WIDTH - 1) - 1)
-                channel_b <= 2**(SAMPLE_WIDTH - 1) - 1;
-            else if (self.channel_b_acc_pre_clamp < -2**(SAMPLE_WIDTH - 1))
-                channel_b <= -2**(SAMPLE_WIDTH - 1);
+            if (self.channel_r_acc_pre_clamp > 2**(SAMPLE_WIDTH - 1) - 1)
+                channel_r <= 2**(SAMPLE_WIDTH - 1) - 1;
+            else if (self.channel_r_acc_pre_clamp < -2**(SAMPLE_WIDTH - 1))
+                channel_r <= -2**(SAMPLE_WIDTH - 1);
             else
-                channel_b <= self.channel_b_acc_pre_clamp;
-
-            if (self.channel_c_acc_pre_clamp > 2**(SAMPLE_WIDTH - 1) - 1)
-                channel_c <= 2**(SAMPLE_WIDTH - 1) - 1;
-            else if (self.channel_c_acc_pre_clamp < -2**(SAMPLE_WIDTH - 1))
-                channel_c <= -2**(SAMPLE_WIDTH - 1);
-            else
-                channel_c <= self.channel_c_acc_pre_clamp;
-
-            if (self.channel_d_acc_pre_clamp > 2**(SAMPLE_WIDTH - 1) - 1)
-                channel_d <= 2**(SAMPLE_WIDTH - 1) - 1;
-            else if (self.channel_d_acc_pre_clamp < -2**(SAMPLE_WIDTH - 1))
-                channel_d <= -2**(SAMPLE_WIDTH - 1);
-            else
-                channel_d <= self.channel_d_acc_pre_clamp;
+                channel_r <= self.channel_r_acc_pre_clamp;
         end
 
-    channel_adder channel_adder (
+    dac_prep dac_prep (
         .*
     );
 
