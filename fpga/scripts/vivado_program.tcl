c0c169e44d1c82a62e23252940741611bc728d1d
Greg Taylor <gtaylor@sonic.net>
2024-05-15 21:39:59 -0400

    change ip for make program

--- fpga/scripts/vivado_program.tcl
+++ fpga/scripts/vivado_program.tcl
@@ -2,7 +2,7 @@ set outputDir build
 
 open_hw
 #connect_hw_server -url localhost:3121
-connect_hw_server -url 192.168.56.1:3121
+connect_hw_server -url 192.168.68.63:3121
 
 current_hw_target [get_hw_targets */xilinx_tcf/Digilent/*]
 set_property PARAM.FREQUENCY 15000000 [get_hw_targets */xilinx_tcf/Digilent/*]
